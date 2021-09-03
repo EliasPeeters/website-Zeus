78f5a2036e551d3013d7088a306e253f2b18e42d
Elias Peeters <Elias.peeters@otto.de>
2021-09-03 07:37:12 +0200

    Added bib

--- assets/js/paperPopUp.js
+++ assets/js/paperPopUp.js
@@ -1,19 +1,30 @@
 let popUp = document.getElementsByClassName('paperPopUp')[0];
 
-function openPaperPopUp(event, element, name) {
+function openPaperPopUp(event, element, name, elements, url) {
     let windowWidth = window.innerWidth;
     popUp.style.display = 'block';
     let left = document.getElementsByClassName(element)[0].offsetLeft;
     
-    if (left + 230 > windowWidth) {
+    if (left + 330 > windowWidth) {
         popUp.style.right = '20px';
     } else {
         popUp.style.left = (left - 5) + 'px';
     }
+    console.log(elements)
+
+    popUp.style.top = (document.getElementsByClassName(element)[0].offsetTop - 5) + 'px';
+    document.getElementsByClassName('paperPopUpTitle')[0].innerHTML = name;
 
     
-    popUp.style.top = (document.getElementsByClassName(element)[0].offsetTop - 5) + 'px';
-    document.getElementsByClassName('paperPopUpTitle')[0].innerHTML = 'Title: ' + name;
+    let popUpContentInnerHTML = '';
+    for (let element in elements) {
+        popUpContentInnerHTML += `<p>${elements[element]}</p>`
+    }
+    if (url != "undefined") {
+        popUpContentInnerHTML += `<button class="buttonPopUp" onclick="openInNewTab('${url}')">Go To Website</button>`
+    }
+    
+    document.getElementsByClassName('popUpContent')[0].innerHTML = popUpContentInnerHTML;
     let heightPopUp = document.getElementsByClassName('popUp')[0].clientHeight;
     popUp.style.height = heightPopUp + 'px';
     
