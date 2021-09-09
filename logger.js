4dc0883fab161cc6f3a09f6d92226822304e8f97
Elias Peeters <Elias.peeters@otto.de>
2021-09-09 14:04:11 +0200

    cleaned up

--- logger.js
+++ logger.js
@@ -6,57 +6,10 @@ if (loggerEnable) {
     console.log('Logger disabled')
 }
 
-const geoip = require('geoip-lite');
-const mysql = require('mysql');
-const main = require('./website-zeus.js');
-const sniffr = require('sniffr')
-
-
 async function log(req, messageID = -1) {
     if (loggerEnable) {
-        let ip = req.connection.remoteAddress;
-        
-        //ip = '37.4.252.185'
-        let geodata = geoip.lookup(ip)
-        const s = new sniffr().sniff(req.headers['user-agent']);
-
-        let data = {
-            table: 'log',
-            browserName: s.browser.name,
-            browserVersion: s.browser.versionString,
-            osName: s.os.name,
-            osVersion: s.os.versionString,
-            date: new Date(),
-            device: s.device.name,
-            referer: req.headers.referer,
-            pageVisited: req.route.path
-        }
-        
-        if (geodata != null) {
-            data.timezone = geodata.timezone;
-            data.area = geodata.area;
-            data.region = geodata.region;
-            data.country = geodata.country;
-        }
-
-        let articleName = req.query.article;
-
-        if (articleName != undefined) {
-            data.articleName = articleName
-        }
-        if (messageID != -1) {
-            data.message_id = messageID;
-        }
-
-        let query = connection.createQueryStringFromObject(data)
-        //console.log(query)
-
-        //console.log(messageID)
-
-        //console.log(query)
-        connection.asyncquery(query);
+        console.log(new Date())
     }
-    //console.log(req.headers.referer)
 }
 
 module.exports = {log}
