4b9c3da3cc81b74bdb7ad53918e574b9c0f97fb7
Elias Peeters <Elias.peeters@otto.de>
2021-09-12 11:41:42 +0200

    Added scroll indicator

---
+++ assets/js/scrollIndicator.js
@@ -0,0 +1,14 @@
+let scrollInFill = document.getElementsByClassName('scrollIndicatorFill')[0];
+
+function fillScrollIndicator(scroll) {
+    var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+        document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+    console.log(scroll/limit);
+    scrollInFill.style.width = ((scroll/(limit - window.innerHeight))*100) + '%'
+}
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    //document.getElementById('navigationBar').style.color = 'white';
+    fillScrollIndicator(scroll)
+});    
