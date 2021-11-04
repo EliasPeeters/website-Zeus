5ed56eb30bd496de25b35fec7a7fcd4451645364
Elias Peeters <Elias.peeters@otto.de>
2021-11-04 21:06:36 +0100

    Created new podcast page

---
+++ assets/configPodcast.js
@@ -0,0 +1,113 @@
+particlesJS("particles-js", 
+                            {
+              "particles": {
+                "number": {
+                  "value": 70,
+                  "density": {
+                    "enable": true,
+                    "value_area": 800
+                  }
+                },
+                "color": {
+                  "value": "#FFFFFF"
+                },
+                "shape": {
+                  "type": "circle",
+                  "stroke": {
+                    "width": 0,
+                    "color": "#1F1D46"
+                  },
+                  "polygon": {
+                    "nb_sides": 5
+                  },
+                  "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                  }
+                },
+                "opacity": {
+                  "value": 0.5,
+                  "random": false,
+                  "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "size": {
+                  "value": 3,
+                  "random": true,
+                  "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "line_linked": {
+                  "enable": true,
+                  "distance": 150,
+                  "color": "#7272A3",
+                  "opacity": 0.4,
+                  "width": 1
+                },
+                "move": {
+                  "enable": true,
+                  "speed": 4,
+                  "direction": "none",
+                  "random": true,
+                  "straight": false,
+                  "out_mode": "out",
+                  "bounce": false,
+                  "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                  }
+                }
+              },
+              "interactivity": {
+                "detect_on": "window",
+                "events": {
+                  "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                  },
+                  "onclick": {
+                    "enable": true,
+                    "mode": "repulse"
+                  },
+                  "resize": true
+                },
+                "modes": {
+                  "grab": {
+                    "distance": 10,
+                    "line_linked": {
+                      "opacity": 1
+                    }
+                  },
+                  "bubble": {
+                    "distance": 10,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 5
+                  },
+                  "repulse": {
+                    "distance": 100,
+                    "duration": 0.4
+                  },
+                  "push": {
+                    "particles_nb": 4
+                  },
+                  "remove": {
+                    "particles_nb": 2
+                  }
+                }
+              },
+              "retina_detect": true
+            });
+            
+            
--- routes/podcast.js
+++ routes/podcast.js
@@ -1,18 +1,9 @@
 let logger = require('../logger.js')
 let request = require('request');
+const puppeteer = require('puppeteer');
 
 app.get('/podcast', function(req,  res){
     //console.log(req);
     res.render('podcast');
     logger.log(req)
 });
-
-app.get('/podcast2', function(req, res) {
-    request('https://redcircle.com/embedded-show-webplayer/ac7565d9-ba20-4c82-8851-933cda3ab5dd?bgColor=%23fcfbf6', (err, result, body) => {
-        if (err) { 
-            console.error(err);
-        }
-        res.send(result)
-        console.log(body);
-    });
-})
