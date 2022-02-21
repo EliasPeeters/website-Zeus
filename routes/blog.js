d918a357bc488b13084ab22ee43be1343cc44a0f
Elias Peeters <Elias.peeters@me.com>
2022-02-21 10:41:09 +0100

    Fixed hera

--- routes/blog.js
+++ routes/blog.js
@@ -18,15 +18,16 @@ app.get('/blog/:article', async function(req, res) {
     console.log(req.params.article)
 
     let queryString = ''
-    for (element in req.params) {
-        queryString += `&${element}=${req.params[element]}`;
+    for (element in req.query) {
+        queryString += `&${element}=${req.query[element]}`;
     }
-
+    console.log(`${serverConnections.blogServer.address}/article/${req.params.article}?server=${serverConnections.blogServer.position}${queryString}`)
     request(`${serverConnections.blogServer.address}/article/${req.params.article}?server=${serverConnections.blogServer.position}${queryString}`, (err, body) => {
         // res.send(body.body)
         if (body.body == 'Article does not exist') {
             res.redirect('/404')
         } else {
+            // console.log(JSON.parse(body.body))
             res.render('article', {article: JSON.parse(body.body)})
         }
     });
